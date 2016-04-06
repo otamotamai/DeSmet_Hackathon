d2222cf023e076f29ccd8669b9af221d1a3cb5fa
sirmilktoast <kfrank@desmet.local>
2016-04-06 16:15:31 -0500

    total starting thingy

--- js/desmet.js
+++ js/desmet.js
@@ -1,4 +1,5 @@
 var menuCounter = 0;
+var cost = 0;
 function changeMenuItems(element){
   $('#main').hide();
   $('#bev').hide();
@@ -10,7 +11,21 @@ function changeMenuItems(element){
 function addItem(item){
   menuCounter++;
   var itemId = 'menuItem'+menuCounter
+
     $('#menu').append('<div id='+itemId+' class="row"><div class="col-md-offset-7">'+item+' <div onClick="removeItem(\''+itemId+'\')">   -- Remove</div></div></div>');
+
+    costs(menuCounter);
+}
+
+function costs(input){
+  if (input == 1){
+    cost = cost + 1.50;
+  }
+  if (input == 2){
+    cost = cost + 1.00;
+  }
+
+  $('#menu').append('<div class="row"><div class="col-md-offset-7">' +cost+ '</div></div>');
 
 }
 
